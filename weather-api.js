6cb833304182f689d4d452d17c3a17058056e850
Nolan Ngo <nolanngo@protonmail.com>
2021-05-10 02:38:58 -0400

    Challange 2 Completed

--- weather-api.js
+++ weather-api.js
@@ -1,12 +1,11 @@
-function getWeather(zip, apikey, success, error) {
+function getWeather(zip, apikey, callback) {
   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apikey}`;
 
   fetch(path).then((res) => res.json()).then((json) => {
     console.log(json);
     console.log(json.main.temp);
-    success(json);
+    callback(json);
   }).catch((err) => {
     console.log(err.message);
-    error(err);
   });
 }
